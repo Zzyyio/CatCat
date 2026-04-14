d2529f862c89780f0b46e6053947dce4a2592361
Zzyyio <zzyyio@outlook.com>
2026-04-14 10:59:04 +0800

    init

---
+++ src/main/java/com/zzyyio/catcat/CatCatDataGenerator.java
@@ -0,0 +1,11 @@
+package com.zzyyio.catcat;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CatCatDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/zzyyio/catcat/CatCatModClient.java
@@ -0,0 +1,11 @@
+package com.zzyyio.catcat;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CatCatModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
