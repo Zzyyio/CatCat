a3b19701849b3adcc2db667fe36c4e6d8cc91d6a
Zzyyio <zzyyio@outlook.com>
2026-04-16 22:32:29 +0800

    [fbdoc]add item (fuel&composting feature)

--- src/main/java/com/zzyyio/catcat/CatCat.java
+++ src/main/java/com/zzyyio/catcat/CatCat.java
@@ -19,7 +19,9 @@ public class CatCat implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-        ModItems.registerItems();
+
+        ModItems.initialize();
+        ModItems.setAttributes();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/zzyyio/catcat/item/ModItems.java
+++ src/main/java/com/zzyyio/catcat/item/ModItems.java
@@ -1,43 +1,59 @@
 package com.zzyyio.catcat.item;
 
 import com.zzyyio.catcat.CatCat;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.client.gui.tab.Tab;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 
 public class ModItems {
     public static final Item test = registerItem("test", new Item(new Item.Settings()));
 
 
-    public static Item registerItems(String id, Item item){
-        return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(),
-                new Identifier(CatCat.MOD_ID,id)),
+    public static void initialize() {
+        // Get the event for modifying entries in the ingredients group.
+        // And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register(itemGroup -> itemGroup.add(ModItems.test));
+
+    }
+
+    public static void setAttributes(){
+        // Add the suspicious substance to the composting registry with a 30% chance of increasing the composter's level.
+        CompostingChanceRegistry.INSTANCE.add(ModItems.test, 0.0f);
+        // Add the suspicious substance to the flammable block registry with a burn time of 30 seconds.
+        // Remember, Minecraft deals with logical based-time using ticks.
+        // 20 ticks = 1 second.
+        FuelRegistry.INSTANCE.add(ModItems.test, 100 * 20);
+    }
+
+    public static Item registerItem(String id, Item item) {
+        return Registry.register(Registries.ITEM,
+                new Identifier(CatCat.MOD_ID, id),
                 item);
-    }
-
-    public static Item registerItem(String id, Item item){
-        return Registry.register(Registries.ITEM, new Identifier(CatCat.MOD_ID,id),item);
-    }
-    public static Item register(String id, Item item) {
-        return register(new Identifier(CatCat.MOD_ID,id), item);
-    }
-
-    public static Item register(Identifier id, Item item) {
-        return register(RegistryKey.of(Registries.ITEM.getKey(), id), item);
-    }
-
-    public static Item register(RegistryKey<Item> key, Item item) {
-        if (item instanceof BlockItem) {
-            ((BlockItem)item).appendBlocks(Item.BLOCK_ITEMS, item);
-        }
-
-        return Registry.register(Registries.ITEM, key, item);
-    }
-
-    public static void registerItems(){
+//    }
+//    public static Item register(String id, Item item) {
+//        return register(new Identifier(CatCat.MOD_ID,id), item);
+//    }
+//
+//    public static Item register(Identifier id, Item item) {
+//        return register(RegistryKey.of(Registries.ITEM.getKey(), id), item);
+//    }
+//
+//    public static Item register(RegistryKey<Item> key, Item item) {
+//        if (item instanceof BlockItem) {
+//            ((BlockItem)item).appendBlocks(Item.BLOCK_ITEMS, item);
+//        }
+//
+//        return Registry.register(Registries.ITEM, key, item);
+//    }
 
     }
 }
