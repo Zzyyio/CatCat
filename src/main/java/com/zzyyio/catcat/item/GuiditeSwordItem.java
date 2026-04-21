4e92524ebe9212d781064082f9bc7bc9538edefa
Zzyyio <zzyyio@outlook.com>
2026-04-21 16:07:14 +0800

    change lightning time per use to 3 & add checks for entity and entity block validation.

--- src/main/java/com/zzyyio/catcat/item/GuiditeSwordItem.java
+++ src/main/java/com/zzyyio/catcat/item/GuiditeSwordItem.java
@@ -1,19 +1,26 @@
 package com.zzyyio.catcat.item;
 
+import com.llamalad7.mixinextras.lib.apache.commons.ObjectUtils;
 import com.zzyyio.catcat.material.GuiditeMaterial;
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.TntEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AirBlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Nullables;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
 
 import static net.minecraft.entity.EntityType.FIREBALL;
 import static net.minecraft.world.World.ExplosionSourceType.TNT;
@@ -31,18 +38,29 @@ public class GuiditeSwordItem extends SwordItem {
             return TypedActionResult.pass(user.getStackInHand(hand));
         }
 
-
-        BlockPos frontOfPlayer = user.getBlockPos().offset(user.getHorizontalFacing(), 10);
-        // Spawn the lightning bolt.
-
         HitResult hit = user.raycast(500.0D, 0.0F, false);
-        LightningEntity lightningBolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
-        lightningBolt.setPos(hit.getPos().getX(),hit.getPos().getY(),hit.getPos().getZ());
-        world.spawnEntity(lightningBolt);
+        ItemStack stack = user.getStackInHand(hand);
+
+        if (hit.getType() == HitResult.Type.MISS) {
+            return TypedActionResult.pass(stack);
+        }
+
+        // 减少 10 点耐久
+        stack.damage(5,user,e -> e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND));
+
+        SpawnLightning(3,hit,world);
         world.createExplosion(null,hit.getPos().getX(),hit.getPos().getY(),hit.getPos().getZ(),10.0f,TNT);
         // Nothing has changed to the item stack,
         // so we just return it how it was.
-        return TypedActionResult.success(user.getStackInHand(hand));
+
+        return TypedActionResult.success(stack);
+    }
+    private void SpawnLightning(int frequency, HitResult hit, World world){
+        for(int i=0;i<frequency;i++){
+            LightningEntity lightningBolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+            lightningBolt.setPos(hit.getPos().getX(),hit.getPos().getY(),hit.getPos().getZ());
+            world.spawnEntity(lightningBolt);
+        }
     }
 
 }
