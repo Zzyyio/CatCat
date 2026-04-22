818d05807568b2ccaa5fe6727058c677c0a824a5
Zzyyio <zzyyio@outlook.com>
2026-04-22 14:03:39 +0800

    modify lamp to 4 states

--- src/main/java/com/zzyyio/catcat/block/PrismarineLampBlock.java
+++ src/main/java/com/zzyyio/catcat/block/PrismarineLampBlock.java
@@ -3,17 +3,14 @@ package com.zzyyio.catcat.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
-import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class PrismarineLampBlock extends Block {
@@ -21,14 +18,14 @@ public class PrismarineLampBlock extends Block {
     public PrismarineLampBlock(Settings settings) {
         super(settings);
         // Set the default state of the block to be deactivated.
-        setDefaultState(getDefaultState().with(ACTIVATED, false));
+        setDefaultState(getDefaultState().with(STRENGTH, 0));
 
     }
-    public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
+    public static final IntProperty STRENGTH = IntProperty.of("strength",0,15);
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(ACTIVATED);
+        builder.add(STRENGTH);
     }
 
     @Override
@@ -38,10 +35,15 @@ public class PrismarineLampBlock extends Block {
             return ActionResult.PASS;
         } else {
             // Get the current value of the "activated" property
-            boolean activated = state.get(ACTIVATED);
+            int strength = state.get(STRENGTH);
 
             // Flip the value of activated and save the new blockstate.
-            world.setBlockState(pos, state.with(ACTIVATED, !activated));
+            if(strength==15){
+                world.setBlockState(pos, state.with(STRENGTH, 0));
+            }
+            else{
+                world.setBlockState(pos, state.with(STRENGTH, strength+5));
+            }
             // Play a click sound to emphasise the interaction.
             world.playSound(player, pos, SoundEvents.BLOCK_COMPARATOR_CLICK, SoundCategory.BLOCKS, 1.0F, 1.0F);
 
@@ -50,9 +52,9 @@ public class PrismarineLampBlock extends Block {
     }
     public static int getLuminance(BlockState currentBlockState) {
         // Get the value of the "activated" property.
-        boolean activated = currentBlockState.get(ACTIVATED);//PrismarineLampBlock.
+        int strength = currentBlockState.get(STRENGTH);//PrismarineLampBlock.
 
         // Return a light level if activated = true
-        return activated ? 150 : 0;
+        return strength;
     }
 }
